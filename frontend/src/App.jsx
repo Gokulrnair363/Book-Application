2cd6e41161f4c5adc3b6382e986790f8a889cfae
Gokulrnair363 <nairgokul20@gmail.com>
2025-08-13 13:45:56 +0530

    added routeprotection,bootswatch theme,updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,45 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext";
+import RouteProtection from "./Routes/RouteProtection";
 
-import './App.css'
 
-function App() {
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import BookList from "./Pages/BookList";
+import BookDetails from "./Pages/BookDetails";
 
+const App = () => {
   return (
-    <>
-     
-    </>
-  )
-}
+    <AuthProvider>
+      <Router>
+        <Routes>
+       
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-export default App
+
+          <Route 
+            path="/books" 
+            element={
+              <RouteProtection>
+                <BookList />
+              </RouteProtection>
+            } 
+          />
+          <Route 
+            path="/books/:id" 
+            element={
+              <RouteProtection>
+                <BookDetails />
+              </RouteProtection>
+            } 
+          />
+
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
